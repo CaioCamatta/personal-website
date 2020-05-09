33a340dc0ae9e0b62a5f05b61e973dca30507fcb
Caio C. Coelho <caiocamattacoelho@hotmail.com>
2020-05-09 13:16:52 -0400

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: "caio-camatta-website",
+        short_name: "website",
+        start_url: "/",
+        background_color: "#F65058",
+        theme_color: "#F65058",
+        display: "minimal-ui",
+        icon: "src/images/icon.png"
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
